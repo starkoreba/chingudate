19bdfe3d2aeec72c382a9bb4cf875aca58b339bd
Maeva De Campos <kulwilcoox@gmail.com>
2022-11-21 19:11:25 +0100

    controller offers + update model/view

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -2,5 +2,5 @@ class Offer < ApplicationRecord
   belongs_to :user
   validates :title, :description, presence: true
   validates :start_date, :end_date, presence: true
-  validates :description, length: { min: 150 }
+  validates :description, length: { minimum: 150 }
 end
