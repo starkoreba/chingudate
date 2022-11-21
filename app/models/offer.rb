7d85558ae25a0393f58266e8fd81dc551a20306a
Maeva De Campos <kulwilcoox@gmail.com>
2022-11-21 18:22:18 +0100

    devise user nickname views + validates user/model + add linkto index offers + routes offers

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,3 +1,6 @@
 class Offer < ApplicationRecord
   belongs_to :user
+  validates :title, :description, presence: true
+  validates :start_date, :end_date, presence: true
+  validates :description, length: { min: 150 }
 end
