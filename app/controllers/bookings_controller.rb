bf5181720a57fe302654f703145a20cd64069e09
Maeva De Campos <kulwilcoox@gmail.com>
2022-11-22 17:53:54 +0100

    add booking create method + controller policy:booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,3 +1,38 @@
 class BookingsController < ApplicationController
+  before_action :set_bookmark, only: :destroy
+  before_action :set_offer, only: %i[new create]
 
+  def index
+    @bookings = current_user.bookings
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.offer = @offer
+    @booking.user = current_user
+    authorize @booking
+    if @booking.save
+      redirect_to bookings_path(@booking)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def set_offer
+    @offer = Offer.find(params[:offer_id])
+  end
 end
