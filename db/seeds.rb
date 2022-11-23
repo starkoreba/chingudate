943d4f05c065eaaefa9bc7ab4b97934668cf0580
Melanie Marechal <mel2012@outlook.fr>
2022-11-23 15:48:16 +0100

    add photo to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 require 'faker'
+require "open-uri"
 
 puts 'Clearing Users'
 User.destroy_all
@@ -20,14 +21,21 @@ puts 'Creating 2 users'
 #     city: 'Bordeaux',
 #     phone_number: '06 66 66 66 66'
 #   )
-user1 = User.create!(email: 'mel@mail.com', password: 'azerty', nickname:'Mel', city:'Bordeaux', phone_number:'06 66 66 66 66')
+
+mel_photo = URI.open("https://res.cloudinary.com/wagon/image/upload/c_fill,g_face,h_200,w_200/v1668689575/h2jd7dodo4ulakrbgt8j.jpg")
+user1 = User.new(email: 'mel@mail.com', password: 'azerty', nickname:'Mel', city:'Bordeaux', phone_number:'06 66 66 66 66')
+user1.photo.attach(io: mel_photo, filename: "mel.png", content_type: "image/png")
+user1.save
 # un pour le parcours presta
 
 offer1 = user1.offers.create!(title:'pour le WE du 15 décembre', description: 'besoin d de quelqu un pour m accompagner a un mariage pour le week end du 15 decembre. je dispose d un vehicule pour le deplacement. personne de la memee tranche d age svp. blablablablablablablablablablablablablabla', location:'Bordeaux', start_date: '2022-12-15 10:00:00', end_date: '2022-12-01 21:00:00')
 
 offer2 = user1.offers.create!(title:'une soirée entre amis', description: 'une soirée organisée avec des amis. Que des couples, je ne veux pas y aller seule, quelqu un pour m accompagnerblablablablablablablablablablablablablabla', location:'Bordeaux', start_date: '2022-11-30 19:00:00', end_date: '2022-11-30 23:00:00')
 
-user2 = User.create!(email: 'rodolphe@mail.com', password: 'azerty', nickname:'Rodolphe', city:'Bordeaux', phone_number:'07 77 77 77 77')
+rodolphe_photo = URI.open("https://pbs.twimg.com/profile_images/618009284891287552/2zZ7qkl1_400x400.jpg")
+user2 = User.new(email: 'rodolphe@mail.com', password: 'azerty', nickname:'Rodolphe', city:'Bordeaux', phone_number:'07 77 77 77 77')
+user2.photo.attach(io: rodolphe_photo, filename: "rodolphe.png", content_type: "image/png")
+user2.save
 # un pour le parcours utilisateur
 
 
